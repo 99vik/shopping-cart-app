84c6746266265ec1e412d8fc5c2c3cc539b2a611
Viktor Miric <viktorm47@gmail.com>
2023-09-26 16:25:32 +0200

    Refactored shop component, styled info module

--- src/components/ShopItems.jsx
+++ src/components/ShopItems.jsx
@@ -1,6 +1,7 @@
 import PropTypes from 'prop-types';
 import { useState } from 'react';
 import CloseIcon from '../assets/icons/close-box.svg';
+import StarIcon from '../assets/icons/star.svg';
 
 function ShopItems({ data }) {
   const [productInfo, setProductInfo] = useState(null);
@@ -13,23 +14,10 @@ function ShopItems({ data }) {
   return (
     <>
       {productInfo && (
-        <>
-          <div className="fixed w-full h-full backdrop-brightness-50 opacity-70 top-0 flex justify-center items-center"></div>
-          <div
-            className="bg-white fixed w-[600px] top-0 translate-y-1/2 
-          right-1/2 translate-x-1/2 flex flex-col items-center p-6 rounded-xl"
-          >
-            <button
-              className="absolute right-2 top-2 hover:opacity-50"
-              onClick={() => {
-                setProductInfo(null);
-              }}
-            >
-              <img src={CloseIcon} alt="close" className="h-10" />
-            </button>
-            <img src={productInfo.image} alt="product image" className="w-40" />
-          </div>
-        </>
+        <ViewInfoModule
+          setProductInfo={setProductInfo}
+          productInfo={productInfo}
+        />
       )}
       <div className="grid max-[570px]:grid-cols-1 max-[850px]:grid-cols-2 min-[1200px]:grid-cols-4 grid-cols-3 gap-6 p-6">
         {items}
@@ -65,12 +53,64 @@ function ShopItem({ item, setProductInfo }) {
   );
 }
 
+function ViewInfoModule({ productInfo, setProductInfo }) {
+  return (
+    <>
+      <div className="fixed w-full h-full backdrop-brightness-50 opacity-70 top-0 flex justify-center items-center"></div>
+      <div
+        className="bg-white fixed max-[680px]:w-[90%] w-[600px] bottom-1/2 translate-y-1/2
+          right-1/2 translate-x-1/2 flex flex-col items-center px-3 md:px-6 py-4 gap-4 rounded-xl"
+      >
+        <button
+          className="absolute right-2 top-2 hover:opacity-50"
+          onClick={() => {
+            setProductInfo(null);
+          }}
+        >
+          <img src={CloseIcon} alt="close" className="h-10" />
+        </button>
+        <p className="text-center text-md  md:text-xl font-semibold px-8">
+          {productInfo.title}
+        </p>
+        <img
+          src={productInfo.image}
+          alt="product image"
+          className="max-w-[220px] w-[50%] aspect-auto"
+        />
+        <div>
+          <p className="text-sm text-neutral-500">Description</p>
+          <p className="text-sm">{productInfo.description}</p>
+        </div>
+        <div className="self-start">
+          <p className="text-sm text-neutral-500">Rating</p>
+          <div className="flex gap-10">
+            <div className="flex justify-center items-center gap-1">
+              <img src={StarIcon} alt="star icon" className="h-6" />
+              <p className="text-lg font-semibold">{productInfo.rating.rate}</p>
+            </div>
+            <div className="flex justify-center items-center gap-1">
+              <p className="text-md">Votes:</p>
+              <p className="text-md">{productInfo.rating.count}</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
 ShopItems.propTypes = {
   data: PropTypes.array,
 };
 
 ShopItem.propTypes = {
   item: PropTypes.object,
+  setProductInfo: PropTypes.func,
+};
+
+ViewInfoModule.propTypes = {
+  setProductInfo: PropTypes.func,
+  productInfo: PropTypes.object,
 };
 
 export default ShopItems;
