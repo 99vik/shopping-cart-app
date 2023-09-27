33b0cc79ca3a923197962a72a6ca5483a0bf56c2
Viktor Miric <viktorm47@gmail.com>
2023-09-27 12:57:08 +0200

    Refactored some code

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -12,7 +12,7 @@ function Cart({ toggleShowCart, cartItems, deleteCartItem }) {
   return (
     <>
       <div className="fixed w-full h-full max-sm:hidden bg-black opacity-20 top-0 flex justify-center items-center"></div>
-      <div className="cart pt-3 pb-10 px-4 bg-white border-l border-neutral-500 md:fixed right-0 grow h-full max-sm:h-1/2 max-sm:w-full w-[400px]">
+      <div className="sm:cart pt-3 pb-10 px-4 bg-white border-l border-neutral-500 md:fixed right-0 grow h-full max-sm:h-1/2 max-sm:w-full w-[400px]">
         <button
           onClick={() => {
             toggleShowCart();
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,46 +7,63 @@ import Cart from './Cart';
 
 function Navbar({ toggleShowCart, numberOfItems, cartItems, deleteCartItem }) {
   const [smallScreenMenu, setSmallScreenMenu] = useState(false);
+
+  function SmallScreenNavMenu({
+    setSmallScreenMenu,
+    toggleShowCart,
+    cartItems,
+    deleteCartItem,
+  }) {
+    return (
+      <div className="small-menu w-full h-full z-10 fixed bg-white flex flex-col justify-start items-center pt-20">
+        <button
+          className="absolute top-2 right-2"
+          onClick={() => {
+            setSmallScreenMenu(false);
+          }}
+        >
+          <img src={CloseIcon} alt="close icon" className="h-14" />
+        </button>
+        <div className="flex justify-evenly w-full">
+          <NavLink to="/">
+            <button
+              onClick={() => {
+                setSmallScreenMenu(false);
+              }}
+              className="text-xl font-semibold bg-neutral-500 w-full py-2 px-8 text-white rounded-lg"
+            >
+              Home
+            </button>
+          </NavLink>
+          <NavLink to="shop">
+            <button
+              onClick={() => {
+                setSmallScreenMenu(false);
+              }}
+              className="text-xl font-semibold bg-neutral-500 w-full py-2 px-8 text-white rounded-lg"
+            >
+              Shop
+            </button>
+          </NavLink>
+        </div>
+        <Cart
+          toggleShowCart={toggleShowCart}
+          cartItems={cartItems}
+          deleteCartItem={deleteCartItem}
+        />
+      </div>
+    );
+  }
+
   return (
     <>
       {smallScreenMenu && (
-        <div className="small-menu w-full h-full z-10 fixed bg-white flex flex-col justify-start items-center pt-20">
-          <button
-            className="absolute top-2 right-2"
-            onClick={() => {
-              setSmallScreenMenu(false);
-            }}
-          >
-            <img src={CloseIcon} alt="close icon" className="h-14" />
-          </button>
-          <div className="flex justify-evenly w-full">
-            <NavLink to="/">
-              <button
-                onClick={() => {
-                  setSmallScreenMenu(false);
-                }}
-                className="text-xl font-semibold bg-neutral-500 w-full py-2 px-8 text-white rounded-lg"
-              >
-                Home
-              </button>
-            </NavLink>
-            <NavLink to="shop">
-              <button
-                onClick={() => {
-                  setSmallScreenMenu(false);
-                }}
-                className="text-xl font-semibold bg-neutral-500 w-full py-2 px-8 text-white rounded-lg"
-              >
-                Shop
-              </button>
-            </NavLink>
-          </div>
-          <Cart
-            toggleShowCart={toggleShowCart}
-            cartItems={cartItems}
-            deleteCartItem={deleteCartItem}
-          />
-        </div>
+        <SmallScreenNavMenu
+          setSmallScreenMenu={setSmallScreenMenu}
+          toggleShowCart={toggleShowCart}
+          cartItems={cartItems}
+          deleteCartItem={deleteCartItem}
+        />
       )}
       <nav className="z-1">
         <div className="flex justify-between items-center px-2 md:px-10 py-4 border border-b-neutral-300">
@@ -98,8 +115,11 @@ function Navbar({ toggleShowCart, numberOfItems, cartItems, deleteCartItem }) {
 }
 
 Navbar.propTypes = {
+  setSmallScreenMenu: PropTypes.func,
   toggleShowCart: PropTypes.func,
   numberOfItems: PropTypes.number,
+  cartItems: PropTypes.array,
+  deleteCartItem: PropTypes.func,
 };
 
 export default Navbar;
